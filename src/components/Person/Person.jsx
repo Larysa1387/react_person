0cc2f5f538c14eb1924c8a7d04af351b5d85a5aa
larysa1387 <kaliuzhna.off@gmail.com>
2025-03-14 11:33:47 +0200

    update Person component

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let partnerRole = 'wife';
+
+  if (person.sex !== 'm') {
+    partnerRole = 'husband';
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age && <p className="Person__age">I am {person.age}</p>}
+      {person.isMarried ? (
+        <p className="Person__partner">
+          {person.partnerName} is my {partnerRole}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
